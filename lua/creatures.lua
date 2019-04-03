f0c59564ec3e06a7a9d976a9963874973e3bab12
Rakaneth <rakaneth@gmail.com>
2019-04-03 10:07:27 -0400

    update data files

---
+++ lua/creatures.lua
@@ -0,0 +1,12 @@
+creatures = {
+    wolf = {
+        name = "Wolf",
+        desc = "A large, grey wolf",
+        glyph = "W",
+        color = "sepia",
+        str = 15,
+        spd = 15,
+        stam = 15,
+        smt = 0
+    }
+}
--- lua/maps.lua
+++ lua/maps.lua
@@ -2,8 +2,45 @@ mapdata = {
     mine = {
         width = 85,
         height = 85,
+        wall_color = "stone",
+        type = "dungeon",
+        name = "Mines (Upper)",
+        down = "lower-mines",
+        up = "world=map"
+    },
+    ["lower-mines"] = {
+        width = 85,
+        height = 85,
+        wall_color = "stone",
+        type = "dungeon",
+        name = "Mine (Lower)",
+        down = "caves",
+        up = "mine"
+    },
+    caves = {
+        width = 100,
+        height = 100,
+        wall_color = "sepia",
+        type = "caves",
+        name = "Caves (Upper)",
+        down = "lower-caves",
+        up = "lower-mines"
+    },
+    ["lower-caves"] = {
+        width = 100,
+        height = 100,
+        wall_color = "sepia",
+        type = "caves",
+        name = "Caves (Lower)",
+        down = "crypt",
+        up = "caves"
+    },
+    crypt = {
+        width = 150,
+        height = 100,
         wall_color = "sepia",
         type = "dungeon",
-        name = "Mines"
+        name = "Crypt",
+        up = "caves"
     }
 }
